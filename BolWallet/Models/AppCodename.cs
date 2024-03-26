1a4c379cb863075a734e8c37981f93c8b87da234
nmoschos <nmoschos@metamind.gr>
2024-03-26 13:25:03 +0200

    Create class to manage app codenames

---
+++ BolWallet/Models/AppCodename.cs
@@ -0,0 +1,12 @@
+﻿namespace BolWallet.Models;
+public class AppCodename
+{
+    public string Codename { get; set; }
+    public bool IsActive { get; set; }
+
+    public AppCodename(string codename, bool isActive)
+    {
+        this.Codename = codename;
+        this.IsActive = isActive;
+    }
+}
